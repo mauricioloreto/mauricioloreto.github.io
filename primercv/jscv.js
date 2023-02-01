5821e99431230969278f76d47c8157ac3c6b7e0f
mauricioloreto <124182813+mauricioloreto@users.noreply.github.com>
2023-02-01 19:43:55 -0300

    initial commit

---
+++ primercv/jscv.js
@@ -0,0 +1,9 @@
+document.getElementById("boton1").addEventListener("click", function(){
+    console.log("Escuela Primiaria Gral. Messi");
+    document.getElementById("demo").innerHTML = "Titulo Educacion Primaria"
+});
+
+document.getElementById("boton2").addEventListener("click", function(){
+    console.log("Escuela Secundaria Don Garrincha");
+    document.getElementById("titu").innerHTML = "Maestro Mayor de Obra"
+});
